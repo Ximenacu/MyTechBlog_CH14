cf569008c9ac3516b7b1bf4c89c4a9895f7d8bf1
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-06-29 21:54:29 -0600

    added comments model and changed index of models folder

---
+++ models/comments.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+
+Comments.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true, 
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        post_id:{
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'posts',
+              key: 'id',
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'users',
+              key: 'id',
+            },
+        },
+    },
+    {
+        hooks: {
+        },
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments',
+    }
+)
+
+module.exports = Comments;
--- models/index.js
+++ models/index.js
@@ -1,9 +1,20 @@
 const Users = require('./users');
 const Posts = require('./posts');
+const Comments = require('./comments')
 
-module.exports = { Users,Posts};
+module.exports = { Users,Posts, Comments};
 
 Users.hasMany(Posts, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
+
+Users.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Posts.hasMany(Comments, {
+    foreignKey: 'post_id', 
+    onDelete: 'CASCADE',
+})
