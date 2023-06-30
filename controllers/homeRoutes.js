57318f534cff08aaaf06d25550835747427969f5
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-06-29 21:43:57 -0600

    home renders posts, when a post is clicked, opens body and add comment form

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,26 +3,30 @@ const { Sequelize } = require('sequelize');
 const Posts = require('../models/posts')
 
 router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
   const loggedIn = req.session.loggedIn;
   try {
     const dbPosts = await Posts.findAll();
     const posts = dbPosts.map((review) => review.get({ plain: true }))
     console.log("POSTS: ----", posts);
-    // const post = {
-    //   id: 2,
-    //   title: 'My Second App',
-    //   content: 'I just deployed my second app in heroku!',
-    //   user_id: 2,
-    // }
-    res.render('home', {posts}); //{ layout: 'main', loggedIn },
+    res.render('home', { posts, layout: 'main', loggedIn }); //,
   } catch (err){
     console.log(err);
     res.status(500).json(err);
   }
-  
-
-  
 });
+
+router.get('/:id', async (req,res) =>{
+  const loggedIn = req.session.loggedIn;
+  try {
+    const dbposts =  await Posts.findByPk(req.params.id);
+    posts = dbposts.get({ plain: true });
+    console.log("-----posts: ", posts);
+    res.render('single',{ posts, layout: 'main', loggedIn } ); //, 
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  
+})
 
 module.exports = router;
